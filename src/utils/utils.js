7da17191a04459af72b79005098b98ec155ab4cc
wscymdb <1724215773@qq.com>
2024-05-08 23:40:00 +0800

    fix:修复已知bug

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,9 +1,14 @@
 const { SERVER_PORT, SERVER_HOST } = require('../config/server.config')
 
 const getFullUrl = (url) => {
-  const urls = url
-    .split(',')
-    .map((item) => `http://${SERVER_HOST}:${SERVER_PORT}/${item}`)
+  const reg = /http:\/\/(.*):(\w+)\//
+  const urls = url.split(',').map((item) => {
+    if (reg.test(item)) {
+      return item
+    }
+
+    return `http://${SERVER_HOST}:${SERVER_PORT}/${item}`
+  })
   return urls.join(',')
 }
 
